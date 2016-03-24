bc5a9c5cc940539388602a5ed8ab45f70b41b2a1
zhenglsPC <zhengls12@qq.com>
2016-03-24 18:52:34 +0800

    bug修改

--- saolei.js
+++ saolei.js
@@ -2,13 +2,12 @@
 
   function saolei(){
 
-
     // 生成雷阵
     function bulei(a,b){
-      var leizheng = new Array();
+      var leizheng = [];
 
       for(var i = 0; i < a; i++){
-        leizheng[i] = new Array();
+        leizheng[i] = [];
         for(var j = 0; j < b; j++){
           if(Math.random() < 0.15626){
             leizheng[i][j] = '-1';
@@ -17,6 +16,7 @@
           }
         }
       }
+
       return leizheng;
     }
 
@@ -34,14 +34,38 @@
             var a = 0,
                 imax = table.length,
                 jmax = table[i].length;
-            if( i >= 1 && i <= imax+1 && j >= 1 && j <= jmax+1 )                 {a += table[i-1][j-1]==-1?1:0; console.log(i,j,i*j,table[i-1][j-1]);}
-            if( i >= 1 && i <= imax+1 && j >= 0 && j <= jmax )                   {a += table[i-1][j]==-1?1:0; console.log(i,j,i*j,table[i-1][j]);}
-            if( i >= 1 && i <= imax+1 && j >= -1 && j <= jmax-1 )                 {a += table[i-1][j+1]==-1?1:0; console.log(i,j,i*j,table[i-1][j+1]);}
-            if( i >= 0 && i <= imax && j >= 1 && j <= jmax+1 )                   {a += table[i][j-1]==-1?1:0; console.log(i,j,i*j,table[i][j-1]);}
-            if( i >= 0 && i <= imax && j >= -1 && j <= jmax-1 )                  {a += table[i][j+1]==-1?1:0; console.log(i,j,i*j,table[i][j+1]);}
-            if( i >= -1 && i < imax-1 && j >= 1 && j <= jmax+1 )                 {a += table[i+1][j-1]==-1?1:0; console.log(i,j,i*j,table[i+1][j-1]);}
-            if( i >= -1 && i < imax-1 && j >= 0 && j <= jmax)                    {a += table[i+1][j]==-1?1:0; console.log(i,j,i*j,table[i+1][j]);}
-            if( i >= -1 && i < imax-1 && j >= -1 && j <= jmax-1)                  {a += table[i+1][j+1]==-1?1:0; console.log(i,j,i*j,table[i+1][j+1]);}
+            if( i >= 1 && i <= imax+1 && j >= 1 && j <= jmax+1 ){
+              a += table[i-1][j-1]==-1?1:0;
+              //console.log(i,j,i*j,table[i-1][j-1]);
+            }
+            if( i >= 1 && i <= imax+1 && j >= 0 && j <= jmax ){
+              a += table[i-1][j]==-1?1:0;
+              //console.log(i,j,i*j,table[i-1][j]);
+            }
+            if( i >= 1 && i <= imax+1 && j >= -1 && j <= jmax-1 ){
+              a += table[i-1][j+1]==-1?1:0;
+              //console.log(i,j,i*j,table[i-1][j+1]);
+            }
+            if( i >= 0 && i <= imax && j >= 1 && j <= jmax+1 ){
+              a += table[i][j-1]==-1?1:0;
+              //console.log(i,j,i*j,table[i][j-1]);
+            }
+            if( i >= 0 && i <= imax && j >= -1 && j <= jmax-1 ){
+              a += table[i][j+1]==-1?1:0;
+              //console.log(i,j,i*j,table[i][j+1]);
+            }
+            if( i >= -1 && i < imax-1 && j >= 1 && j <= jmax+1 ){
+              a += table[i+1][j-1]==-1?1:0;
+              //console.log(i,j,i*j,table[i+1][j-1]);
+            }
+            if( i >= -1 && i < imax-1 && j >= 0 && j <= jmax){
+              a += table[i+1][j]==-1?1:0;
+              //console.log(i,j,i*j,table[i+1][j]);
+            }
+            if( i >= -1 && i < imax-1 && j >= -1 && j <= jmax-1){
+              a += table[i+1][j+1]==-1?1:0;
+              //console.log(i,j,i*j,table[i+1][j+1]);
+            }
 
             table[i][j] = ''+a;
           }
@@ -53,21 +77,22 @@
 
     // 更新面板
     function renewPanel(table){
-      var width = table[0].length * 20;
       var htmlstr = '';
-      $('#wrapper').width(width + table[0].length*2);
       for( var i in table){
         for ( var j in table[i] ){
           if( table[i][j] > 10 && table[i][j] < 18){
             htmlstr += '<div data-i="' + i + '" data-j=' + j + '>' + (Math.floor(table[i][j])-10) + '</div>';
           }else if( table[i][j] == 10 ){
             htmlstr += '<div data-i="' + i + '" data-j=' + j + '></div>';
+            //htmlstr += '<div data-i="' + i + '" data-j=' + j + '>'+table[i][j]+'</div>';
           }else if( table[i][j] == 9 ){
-            htmlstr += '<div class="lei" data-i="' + i + '" data-j=' + j + '>*</div>';
+            htmlstr += '<div class="lei" data-i="' + i + '" data-j=' + j + '>雷</div>';
           }else if(  table[i][j] > 30 && table[i][j] <= 49 ){
-            htmlstr += '<div class="leiclose" data-i="' + i + '" data-j=' + j + '>*</div>';
+            //htmlstr += '<div class="leiclose" data-i="' + i + '" data-j=' + j + '>'+table[i][j]+'</div>';
+            htmlstr += '<div class="leiclose" data-i="' + i + '" data-j=' + j + '>雷</div>';
           }else {
             htmlstr += '<div class="close" data-i="' + i + '" data-j=' + j + '></div>';
+            //htmlstr += '<div class="close" data-i="' + i + '" data-j=' + j + '>'+table[i][j]+'</div>';
           }
 
         }
@@ -78,37 +103,41 @@
 
     // 扫雷
     function shaolei(table){
+      $('#wrapper').off("mousedown");
       $('#wrapper').on('mousedown','.close',function(e){
-          if( e.button == 0 ){
-            var i = $(this).data('i');
-            var j = $(this).data('j');
+        //console.log(e);
+        var i = $(this).data('i');
+        var j = $(this).data('j');
+        if( e.button == 0 ){
 
-            if(table[i][j] < 8) {
-              table[i][j] = Math.floor(table[i][j]) + 10;
-            }
-
-            if(table[i][j] == 10){
-              showEmpty(i,j,table);
-            }
-
-            if(table[i][j] == 9){
-              alert('YOU LOST!!!');
-              showlei(table);
-            }
-
-            renewPanel(table);
-            iswin(table);
-          }else{
-              $(this).addClass('leiclose').removeClass('close').html('*');
-              table[i][j] = Math.floor(table[i][j]) + 30;
+          if(table[i][j] < 8) {
+            table[i][j] = Math.floor(table[i][j]) + 10;
           }
-      })
+
+          if(table[i][j] == 10){
+            showEmpty(i,j,table);
+          }
+
+          if(table[i][j] == 9){
+            alert('YOU LOST!!!');
+            showlei(table);
+          }
+
+          renewPanel(table);
+          iswin(table);
+        }
+        else if(e.button == 2){
+          table[i][j] = parseInt(table[i][j]) + 40;
+          $(this).addClass('leiclose').removeClass('close').html('雷');
+        }
+      });
+
       $('#wrapper').on('mousedown','.leiclose',function(e){
         var i = $(this).data('i');
         var j = $(this).data('j');
         if( e.button == 2 ){
+          table[i][j] = parseInt(table[i][j]) - 40;
           $(this).removeClass('leiclose').addClass('close').html('');
-          table[i][j] = Math.floor(table[i][j]) - 30;
         }
       })
     }
@@ -144,6 +173,7 @@
       //     }
       //   }
       // }
+
       for(var i in table){
         for(var j in table[i]){
           if ( table[i][j] == 0 ) {
@@ -156,13 +186,13 @@
 
     // 是否胜利
     function iswin(table){
-        for(var i in table){
-          for(var j in table[i]){
-            if ( table[i][j] > 0 && table[i][j] < 10 ) {
-              return false;
-            }
+      for(var i in table){
+        for(var j in table[i]){
+          if ( table[i][j] > 0 && table[i][j] < 10 ) {
+            return false;
           }
         }
+      }
       alert('YOU WIN!!!');
     }
 
@@ -170,7 +200,10 @@
       for(var i in table){
         for(var j in table[i]){
           if ( table[i][j] == -1 ) {
-            table[i][j] = Math.floor(table[i][j])+10;
+            table[i][j] = parseInt(table[i][j])+10;
+          }
+          else if(table[i][j] == 39){
+            table[i][j] = 9;
           }
         }
       }
@@ -180,6 +213,8 @@
     // 初始化
     this.init = function(){
       var place = bulei(8,8);
+      var width = 8 * (30+2);
+      $('#wrapper').width(width);
       addshuzi(place);
       renewPanel(place);
       shaolei(place);
